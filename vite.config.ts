166c60bf0f589a686db56c576ebbc9a3eb7ea32e
Nathália <nathaliarsantos@live.com>
2025-06-12 16:11:59 -0300

    public web site

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/nathalia-portfolio-elegance/",
   server: {
     host: "::",
     port: 8080,
